8d5e69fe11a77cc2f995f64eb677cad551766230
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-02-15 00:35:47 -0500

    bug fixed, new bug noted for later

--- day5/part1.py
+++ day5/part1.py
@@ -1,5 +1,8 @@
 input = open("testinput.txt", "r").read().splitlines()
 
+
+#MISTAKE: I ONLY ADD VENTS TO hasVent WHICH ARE FROM x1,y2 to x2,y2 when x2> x1 and y2>y1 
+#need to evaluate which way the vent vector points and record that correspondinly
 hasVent = []
 for i in input:
     firstHalf = i.split("->")[0]
@@ -29,28 +32,28 @@ samey = 0
 xCoord = int(hasVent[0][0])
 yCoord = int(hasVent[0][1])
 repeat = []
+counter = 0
 for coord in hasVent:
-    xCoord = int(hasVent[0][0])
-    yCoord = int(hasVent[0][1])
-
+    xCoord = int(coord[0])
+    yCoord = int(coord[1])
+    print("Outside coord: " + str(coord))
+    hasVent.remove(coord)
     for coordEmbedded in hasVent:
-        if (first == 0):
-            first = 1
-            continue
-        if (xCoord == coord[0]):
+        #print("Embedded coord: " + str(coordEmbedded))
+        if (xCoord == coordEmbedded[0]):
             samex = 1
-        if (yCoord == coord[1]):
+        if (yCoord == coordEmbedded[1]):
             samey = 1
         if (samex == 1 and samey == 1):
+            print("hi")
             if ([xCoord, yCoord] in repeat):
                 pass
             else:
                 count = count + 1
-                repeat.append[xCoord,yCoord]
-    
-    samex = 0
-    samey = 0
-    xCoord = int(coord[0])
-    yCoord = int(coord[1])
+                repeat.append([xCoord,yCoord])
+        samex = 0
+        samey = 0
+    hasVent.insert(counter, coord)
+    counter += 1
     
 print("Count: " + str(count))
