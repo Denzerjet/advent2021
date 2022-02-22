6b882222c3e04bc5797768a22e783749dbb815ac
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-02-21 23:58:57 -0500

    method is too inefficient
    need to optimize or find different way

--- day5/part2.py
+++ day5/part2.py
@@ -36,18 +36,34 @@ for i in input:
             for x in range (x1, x2+1):
                 hasVent.append([x,y1])
     #can't do this b/c x2-x1 can = 0
-    if (abs(math.atan((y2-y1)/(x2-x1))) == math.pi/4):
-        if (x1 > x2):
-            if(y2 > y1):
-                #if the x coord coord of first pt is greater and the y coord of the 2nd coord is greater then the line 
-                pass
+    if (x2 != x1):
+        if (abs(math.atan((y2-y1)/(x2-x1))) == math.pi/4):
+            if (x2 > x1):
+                if(y2 > y1):
+                    #if the x coord coord of first pt is greater and the y coord of the 2nd coord is greater then the line 
+                    deltaX = x2-x1
+                    i = 0
+                    while (i < deltaX):
+                        hasVent.append([x1+i,y1+i])
+                        i += 1
+                else:
+                    deltaX = x2-x1
+                    j = 0
+                    while (j < deltaX):
+                        hasVent.append([x1+j,y2+j])
             else:
-                pass
-        else:
-            if(y2 > y1):
-                pass
-            else:
-                pass
+                if(y2 > y1):
+                    deltaX2 = x1-x2
+                    k = 0
+                    while (k < deltaX2):
+                        hasVent.append([x2+k,y1+k])
+                        k += 1
+                else:
+                    deltaX2 = x1-x2
+                    m = 0
+                    while (m < deltaX2):
+                        hasVent.append([x2+m,y2+m])
+                        m += 1
 
 count = 0
 first = 0
