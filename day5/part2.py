79f2a1fa83efc46b74094525d5b65ab88bc159ec
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-02-22 23:58:39 -0500

    fix: adding diagonals

--- day5/part2.py
+++ day5/part2.py
@@ -1,6 +1,6 @@
 import math
 
-input = open("input.txt", "r").read().splitlines()
+input = open("testinput.txt", "r").read().splitlines()
 
 hasVent = []
 for i in input:
@@ -35,35 +35,33 @@ for i in input:
         else: 
             for x in range (x1, x2+1):
                 hasVent.append([x,y1])
-    #can't do this b/c x2-x1 can = 0
-    if (x2 != x1):
-        if (abs(math.atan((y2-y1)/(x2-x1))) == math.pi/4):
-            if (x2 > x1):
-                if(y2 > y1):
-                    #if the x coord coord of first pt is greater and the y coord of the 2nd coord is greater then the line 
-                    deltaX = x2-x1
-                    i = 0
-                    while (i < deltaX):
-                        hasVent.append([x1+i,y1+i])
-                        i += 1
-                else:
-                    deltaX = x2-x1
-                    j = 0
-                    while (j < deltaX):
-                        hasVent.append([x1+j,y2+j])
-            else:
-                if(y2 > y1):
-                    deltaX2 = x1-x2
-                    k = 0
-                    while (k < deltaX2):
-                        hasVent.append([x2+k,y1+k])
-                        k += 1
-                else:
-                    deltaX2 = x1-x2
-                    m = 0
-                    while (m < deltaX2):
-                        hasVent.append([x2+m,y2+m])
-                        m += 1
+    else:
+        if (y2 > y1):
+            x3 = max(x1,x2) - min(x1,x2)
+            k = 0
+            while (k <= x3):
+                hasVent.append([min(x1,x2)+k,min(y1,y2)+k])
+                k += 1
+        else:
+            x3 = max(x1,x2) - min(x1,x2)
+            k = 0
+            if (x2 > x1 and y2 > y1):
+                while (k <= x3):
+                    hasVent.append([x1+k,y1+k])
+                    k += 1
+            if (x2 > x1 and y1 > y2):
+                while (k <= x3):
+                    hasVent.append([x1+k,y1-k])
+                    k += 1
+            if (x1 > x2 and y2 > y1):
+                while (k <= x3):
+                    hasVent.append([x2+k,y2-k])
+                    k += 1
+            if (x1 > x2 and y1 > y2):
+                while (k <= x3):
+                    hasVent.append([x2+k,y2+k])
+                    k += 1
+
 
 count = 0
 first = 0
