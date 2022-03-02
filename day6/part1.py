15e290d3a6ab14e2cfd9c87b6cde17e82928c541
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-03-01 23:59:31 -0500

    added iterations

--- day6/part1.py
+++ day6/part1.py
@@ -2,5 +2,15 @@ input = open("input.txt", "r").read()
 input_numbers = input.split(",")
 
 gen = () #tuple 
+lantern_fish = []
+
 for num in input_numbers:
-    gen = gen + (int(num),) #have to use comma or else it isn't iterable 
+    lantern_fish.append(int(num))
+
+for i in range (80):
+    for j in len(lantern_fish):
+        if (lantern_fish[i] == 0):
+            lantern_fish[i] = 6
+            lantern_fish.append(8)
+        else:
+            lantern_fish[i] -= 1
