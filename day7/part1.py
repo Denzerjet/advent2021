ca2d9e494e7e852f9a6cb31c95c4ab95f3fe2c16
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-03-03 02:14:46 -0500

    Create part1.py

---
+++ day7/part1.py
@@ -0,0 +1,24 @@
+from turtle import pos
+
+
+input = open("input.txt", "r").read()
+positions = input.split(",")
+
+min_sum = 0
+sum_iterator = 0
+max_pos = int(positions[0])
+
+for crab_pos in positions:
+    if(int(crab_pos) > max_pos):
+        max_pos = int(crab_pos)
+
+#store the smallest sum only
+for i in range (max_pos):
+    for crab_pos in positions:
+        sum_iterator += abs(i - int(crab_pos))
+    if (i==0):
+        min_sum = sum_iterator
+    if ((i!=0) and (min_sum > sum_iterator)):
+        min_sum = sum_iterator
+    sum_iterator = 0
+print(min_sum)
