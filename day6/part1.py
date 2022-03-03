a8f8b832754ff3bb67bf215453656cdd2f18e84f
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-03-02 23:52:14 -0500

    Update part1.py

--- day6/part1.py
+++ day6/part1.py
@@ -7,10 +7,13 @@ lantern_fish = []
 for num in input_numbers:
     lantern_fish.append(int(num))
 
+for fish in lantern_fish:
+        print(fish)
 for i in range (80):
-    for j in len(lantern_fish):
-        if (lantern_fish[i] == 0):
-            lantern_fish[i] = 6
+    for j in range(len(lantern_fish)):
+        if (lantern_fish[j] == 0):
+            lantern_fish[j] = 6
             lantern_fish.append(8)
         else:
-            lantern_fish[i] -= 1
+            lantern_fish[j] -= 1
+print(len(lantern_fish))
