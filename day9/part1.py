314a3586c1f2e671417e03d7101f5905e865c75b
Denzerjet <37031950+Denzerjet@users.noreply.github.com>
2022-03-13 23:59:47 -0700

    Update part1.py

--- day9/part1.py
+++ day9/part1.py
@@ -4,6 +4,11 @@ length = 0
 inputTuple = tuple(tuple(i) for i in input)
 sum = 0
 
+#outer refers to each line of the input being stored as a single tuple
+#inner refers to each of the numbers of each line of the input being stored as singular tuples inside of a tuple
+# that encompasses all of them (like a 2d array)
+
+#note: have to iterate by a number in range (...) because we tuples can't be converted to ints for comparisons
 for outer in range (0, len(inputTuple)):
     largest = True
     for inner in range(0, len(inputTuple[outer])):
@@ -11,20 +16,18 @@ for outer in range (0, len(inputTuple)):
         #make a comparison to the right to the right (inner + 1) if inner != len(inputTuple[outer]) - 1
         #set the variable to false if it isn't the largest to it's adjacents
         #if the variable is true add it plus one to sum
-        if (inner != 0 and inner != len(inputTuple[outer]) - 1):
-            #if not on the left edge
+        if (inner != 0):
+            #if not on the left edge, compare to an element on the left
             largest = inputTuple[outer][inner] < inputTuple[outer][inner + 1]
-            pass
-        elif (inner =! len(inputTuple[outer]) - 1):
-            #if not on the right edge
+        if (inner =! len(inputTuple[outer]) - 1):
+            #if not on the right edge, compare to an element 
             pass
         if (outer == 0):
             #top edge
             pass
-        elif (outer == len(inputTuple) - 1):
+        if (outer == len(inputTuple) - 1):
             #bottom edge
             pass
-
         if (largest):
             #add it plus one to sum
             pass
